e5dd16f4f3dc72e9bbfe38dc75beb3bf038a5481
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-02-19 15:38:01 -0300

    Create sp_cria_tabelas.sql

---
+++ sp_cria_tabelas.sql
@@ -0,0 +1,35 @@
+CREATE OR ALTER PROCEDURE SP_CRIA_TABELAS_DW AS
+BEGIN
+
+    ----TABELA FILIAL
+    IF NOT EXISTS (SELECT 1	FROM INFORMATION_SCHEMA.TABLES	WHERE TABLE_NAME = 'FILIAL')
+    BEGIN
+		CREATE TABLE [dbo].[Filial] (
+        [Cod.Filial] NCHAR (2)      NOT NULL,
+        [Empresa]    NVARCHAR (150) NULL,
+        [Filial]     NVARCHAR (50)  NULL,
+        [Ordem]      SMALLINT       NOT NULL,
+        CONSTRAINT [PK_Filial] PRIMARY KEY CLUSTERED ([Cod.Filial] ASC)
+    );
+
+    CREATE NONCLUSTERED INDEX [Index_Filial_2]
+        ON [dbo].[Filial]([Empresa] ASC);
+
+
+    CREATE NONCLUSTERED INDEX [Index_Filial_1]
+        ON [dbo].[Filial]([Cod.Filial] ASC);
+
+
+    CREATE NONCLUSTERED INDEX [Index_Filial_5]
+        ON [dbo].[Filial]([Cod.Filial] ASC, [Empresa] ASC, [Filial] ASC);
+
+
+    CREATE NONCLUSTERED INDEX [Index_Filial_4]
+        ON [dbo].[Filial]([Ordem] ASC);
+
+
+    CREATE NONCLUSTERED INDEX [Index_Filial_3]
+        ON [dbo].[Filial]([Filial] ASC)
+    END
+
+END
