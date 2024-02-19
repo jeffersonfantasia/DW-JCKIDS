f98c9482816a21ce9b2be88ea0f5333faae2440a
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-02-19 15:37:52 -0300

    Update sp_carrega_Filial.sql

--- Filial/sp_carrega_Filial.sql
+++ Filial/sp_carrega_Filial.sql
@@ -3,40 +3,8 @@ CREATE OR ALTER PROCEDURE SP_CARREGA_FILIAL
 	@EMPRESA NVARCHAR(150),
 	@FILIAL NVARCHAR(25)
 AS
-BEGIN
-	----Verifica se a tabela existe
-	IF NOT EXISTS (SELECT 1	FROM INFORMATION_SCHEMA.TABLES	WHERE TABLE_NAME = 'FILIAL')
-    BEGIN
-		CREATE TABLE [dbo].[Filial] (
-    [Cod.Filial] NCHAR (2)      NOT NULL,
-    [Empresa]    NVARCHAR (150) NULL,
-    [Filial]     NVARCHAR (50)  NULL,
-    [Ordem]      SMALLINT       NOT NULL,
-    CONSTRAINT [PK_Filial] PRIMARY KEY CLUSTERED ([Cod.Filial] ASC)
-);
-
-CREATE NONCLUSTERED INDEX [Index_Filial_2]
-    ON [dbo].[Filial]([Empresa] ASC);
-
-
-CREATE NONCLUSTERED INDEX [Index_Filial_1]
-    ON [dbo].[Filial]([Cod.Filial] ASC);
-
-
-CREATE NONCLUSTERED INDEX [Index_Filial_5]
-    ON [dbo].[Filial]([Cod.Filial] ASC, [Empresa] ASC, [Filial] ASC);
-
-
-CREATE NONCLUSTERED INDEX [Index_Filial_4]
-    ON [dbo].[Filial]([Ordem] ASC);
-
-
-CREATE NONCLUSTERED INDEX [Index_Filial_3]
-    ON [dbo].[Filial]([Filial] ASC);
-	END
-    
+BEGIN    
 	----Faz a transformação das informações
-	BEGIN
        DECLARE @ORDEM SMALLINT
        SET @ORDEM = CONVERT(SMALLINT, @CODFILIAL)
 	   SET @EMPRESA  = CASE WHEN @EMPRESA IS NULL THEN 'JC BROTHERS' ELSE @EMPRESA END
@@ -51,6 +19,5 @@ CREATE NONCLUSTERED INDEX [Index_Filial_3]
 			BEGIN
 			INSERT INTO FILIAL ([Cod.Filial], Empresa, Filial, Ordem)
 			VALUES (@CODFILIAL, @EMPRESA, @FILIAL, @ORDEM)
-		END
 	END
 END
