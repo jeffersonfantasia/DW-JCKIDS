642f66caf88d97aabfec86a126c3b0a1ef9d7d05
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-04-08 16:24:49 -0300

    Update 01_PRC_SINC_PRECO_VENDA.sql

--- Preco_Venda/01_PRC_SINC_PRECO_VENDA.sql
+++ Preco_Venda/01_PRC_SINC_PRECO_VENDA.sql
@@ -1,29 +1,34 @@
 CREATE OR REPLACE PROCEDURE PRC_SINC_PRECO_VENDA AS
 BEGIN
   -- Insere os resultados novos ou alterados na tabela TEMP
-  INSERT INTO TEMP_PCTABPR
-    (CODPROD, NUMREGIAO, REGIAO, PRECOVENDA, MARGEMIDEAL)
-    SELECT P.CODPROD,
-           P.NUMREGIAO,
-           R.REGIAO,
-           P.PVENDA PRECOVENDA,
-           P.MARGEM MARGEMIDEAL
-      FROM PCTABPR P
-      JOIN PCREGIAO R ON R.NUMREGIAO = P.NUMREGIAO
+  INSERT INTO TEMP_PRECO_VENDA
+    (CODPROD,
+     NUMREGIAO,
+     PRECOVENDA,
+     MARGEMIDEAL)
+    WITH PRECO_VENDA AS
+     (SELECT P.CODPROD,
+             P.NUMREGIAO,
+             P.PVENDA PRECOVENDA,
+             P.MARGEM MARGEMIDEAL
+        FROM PCTABPR P
+        JOIN PCREGIAO R ON R.NUMREGIAO = P.NUMREGIAO
+       WHERE NVL(P.PVENDA, 0) > 0)
+    SELECT P.*
+      FROM PRECO_VENDA P
       LEFT JOIN BI_SINC_PRECO_VENDA S ON S.CODPROD = P.CODPROD
                                      AND S.NUMREGIAO = P.NUMREGIAO
-     WHERE NVL(P.PVENDA, 0) > 0
-       AND (S.DT_UPDATE IS NULL OR S.REGIAO <> R.REGIAO OR
-            S.PRECOVENDA <> P.PVENDA OR S.MARGEMIDEAL <> P.MARGEM);
+     WHERE S.DT_UPDATE IS NULL
+        OR S.PRECOVENDA <> P.PRECOVENDA
+        OR S.MARGEMIDEAL <> P.MARGEMIDEAL;
 
   -- Atualiza ou insere os resultados na tabela BI_SINC conforme as condições mencionadas
-  FOR temp_rec IN (SELECT * FROM TEMP_PCTABPR)
+  FOR temp_rec IN (SELECT * FROM TEMP_PRECO_VENDA)
   
   LOOP
     BEGIN
       UPDATE BI_SINC_PRECO_VENDA
-         SET REGIAO      = temp_rec.REGIAO,
-             PRECOVENDA  = temp_rec.PRECOVENDA,
+         SET PRECOVENDA  = temp_rec.PRECOVENDA,
              MARGEMIDEAL = temp_rec.MARGEMIDEAL
        WHERE CODPROD = temp_rec.CODPROD
          AND NUMREGIAO = temp_rec.NUMREGIAO;
@@ -33,14 +38,12 @@ BEGIN
         INSERT INTO BI_SINC_PRECO_VENDA
           (CODPROD,
            NUMREGIAO,
-           REGIAO,
            PRECOVENDA,
            MARGEMIDEAL,
            DT_UPDATE)
         VALUES
           (temp_rec.CODPROD,
            temp_rec.NUMREGIAO,
-           temp_rec.REGIAO,
            temp_rec.PRECOVENDA,
            temp_rec.MARGEMIDEAL,
            SYSDATE);
@@ -49,7 +52,7 @@ BEGIN
       WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Erro encontrado: ' || SQLERRM);
         RAISE_APPLICATION_ERROR(-20000,
-                                'Erro durante a criação da tabela: ' ||
+                                'Erro durante a insercao na tabela: ' ||
                                 SQLERRM);
     END;
   END LOOP;
@@ -57,5 +60,5 @@ BEGIN
   COMMIT;
 
   -- Exclui os registros da tabela temporária TEMP criada;
-  EXECUTE IMMEDIATE 'DELETE TEMP_PCTABPR';
+  EXECUTE IMMEDIATE 'DELETE TEMP_PRECO_VENDA';
 END;
