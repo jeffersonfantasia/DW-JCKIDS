8102ed5706f6eef212be46d217e20c2eeaf1ae2d
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-04-25 16:50:50 -0300

    Update coluna ordem

--- Filial/01_PRC_SINC_FILIAL.sql
+++ Filial/01_PRC_SINC_FILIAL.sql
@@ -2,12 +2,16 @@ CREATE OR REPLACE PROCEDURE PRC_SINC_FILIAL AS
 BEGIN
 
   -- Insere os resultados novos ou alterados na tabela TEMP
-  INSERT INTO TEMP_PCFILIAL
-    (CODFILIAL, EMPRESA, FILIAL)
+  INSERT INTO TEMP_FILIAL
+    (CODFILIAL,
+     EMPRESA,
+     FILIAL,
+     ORDEM)
     WITH FILIAIS AS
      (SELECT F.CODIGO CODFILIAL,
              NVL(F.NOMEREMETENTE, 'JC BROTHERS') EMPRESA,
-             NVL(F.FANTASIA, 'JC BROTHERS') FILIAL
+             NVL(F.FANTASIA, 'JC BROTHERS') FILIAL,
+             TO_NUMBER(F.CODIGO) ORDEM
         FROM PCFILIAL F)
     SELECT F.*
       FROM FILIAIS F
@@ -15,32 +19,38 @@ BEGIN
      WHERE S.DT_UPDATE IS NULL
         OR NVL(S.EMPRESA, '0') <> F.EMPRESA
         OR NVL(S.FILIAL, '0') <> F.FILIAL;
+
   -- Atualiza ou insere os resultados na tabela BI_SINC conforme as condições mencionadas
-  FOR temp_rec IN (SELECT * FROM TEMP_PCFILIAL)
+  FOR temp_rec IN (SELECT * FROM TEMP_FILIAL)
   
   LOOP
     BEGIN
       UPDATE BI_SINC_FILIAL
          SET EMPRESA   = temp_rec.EMPRESA,
              FILIAL    = temp_rec.FILIAL,
+             ORDEM     = temp_rec.ORDEM,
              DT_UPDATE = SYSDATE
        WHERE CODFILIAL = temp_rec.CODFILIAL;
     
-      IF SQL%NOTFOUND
-      THEN
+      IF SQL%NOTFOUND THEN
         INSERT INTO BI_SINC_FILIAL
-          (CODFILIAL, EMPRESA, FILIAL, DT_UPDATE)
+          (CODFILIAL,
+           EMPRESA,
+           FILIAL,
+           ORDEM,
+           DT_UPDATE)
         VALUES
           (temp_rec.CODFILIAL,
            temp_rec.EMPRESA,
            temp_rec.FILIAL,
+           temp_rec.ORDEM,
            SYSDATE);
       END IF;
     EXCEPTION
       WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Erro encontrado: ' || SQLERRM);
         RAISE_APPLICATION_ERROR(-20000,
-                                'Erro durante a criação da tabela: ' ||
+                                'Erro durante a insercao na tabela: ' ||
                                 SQLERRM);
     END;
   END LOOP;
@@ -48,5 +58,5 @@ BEGIN
   COMMIT;
 
   -- Exclui os registros da tabela temporária TEMP criada;
-  EXECUTE IMMEDIATE 'DELETE TEMP_PCFILIAL';
+  EXECUTE IMMEDIATE 'DELETE TEMP_FILIAL';
 END;
