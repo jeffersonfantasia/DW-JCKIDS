7bcb55c83e654ec237f099967a8c1b2995ae54e5
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-05-21 18:30:41 -0300

    Update 02_PRC_SINC_PAGAR_FORNECEDOR.sql

--- Lancamentos_Pagar/02_PRC_SINC_PAGAR_FORNECEDOR.sql
+++ Lancamentos_Pagar/02_PRC_SINC_PAGAR_FORNECEDOR.sql
@@ -1,4 +1,7 @@
 CREATE OR REPLACE PROCEDURE PRC_SINC_PAGAR_FORNECEDOR AS
+
+  vDATA_PAG_INCREMENTAL DATE := TRUNC(SYSDATE) - 30;
+
 BEGIN
   -- Insere os resultados novos ou alterados na tabela TEMP
   INSERT INTO TEMP_PAGAR_FORNECEDOR
@@ -8,9 +11,10 @@ BEGIN
      DTVENCIMENTO,
      DTPAGAMENTO,
      VALOR,
-		 VALORAPAGAR,
+     VALORAPAGAR,
      CODFORNEC,
-     NOTA)
+     NOTA,
+     CODBANCO)
     WITH LANCAMENTOS AS
      (SELECT L.RECNUM,
              L.CODFILIAL,
@@ -18,25 +22,26 @@ BEGIN
              L.DTVENCIMENTO,
              L.DTPAGAMENTO,
              L.VALOR,
-						 L.VALORAPAGAR,
+             L.VALORAPAGAR,
              L.CODFORNEC,
-             (L.NUMNOTA || '-' || L.DUPLICATA) NOTA
+             (L.NUMNOTA || '-' || L.DUPLICATA) NOTA,
+             L.CODBANCO
         FROM BI_SINC_LANC_PAGAR L
        WHERE L.TIPOPARCEIRO = 'F'
          AND L.CODCONTA = 100001)
     SELECT L.*
       FROM LANCAMENTOS L
       LEFT JOIN BI_SINC_PAGAR_FORNECEDOR S ON S.RECNUM = L.RECNUM
-     WHERE S.DT_UPDATE IS NULL
-        OR NVL(S.DTVENCIMENTO,'01/01/1899') <> L.DTVENCIMENTO
-        OR NVL(S.DTPAGAMENTO,'01/01/1899') <> L.DTPAGAMENTO
-        OR S.VALOR <> L.VALOR
-				OR S.VALORAPAGAR <> L.VALORAPAGAR
-        OR S.NOTA <> L.NOTA;
+     WHERE (L.DTPAGAMENTO >= vDATA_PAG_INCREMENTAL OR L.DTPAGAMENTO IS NULL)
+       AND (S.DT_UPDATE IS NULL OR
+           NVL(S.DTVENCIMENTO, '01/01/1899') <> L.DTVENCIMENTO OR
+           NVL(S.DTPAGAMENTO, '01/01/1899') <> L.DTPAGAMENTO OR
+           S.VALOR <> L.VALOR OR S.VALORAPAGAR <> L.VALORAPAGAR OR
+           S.NOTA <> L.NOTA);
 
   -- Atualiza ou insere os resultados na tabela BI_SINC conforme as condições mencionadas
   FOR temp_rec IN (SELECT * FROM TEMP_PAGAR_FORNECEDOR)
-  
+
   LOOP
     BEGIN
       UPDATE BI_SINC_PAGAR_FORNECEDOR
@@ -45,12 +50,13 @@ BEGIN
              DTVENCIMENTO  = temp_rec.DTVENCIMENTO,
              DTPAGAMENTO   = temp_rec.DTPAGAMENTO,
              VALOR         = temp_rec.VALOR,
-						 VALORAPAGAR   = temp_rec.VALORAPAGAR,
+             VALORAPAGAR   = temp_rec.VALORAPAGAR,
              CODFORNEC     = temp_rec.CODFORNEC,
              NOTA          = temp_rec.NOTA,
+             CODBANCO      = temp_rec.CODBANCO,
              DT_UPDATE     = SYSDATE
        WHERE RECNUM = temp_rec.RECNUM;
-    
+
       IF SQL%NOTFOUND
       THEN
         INSERT INTO BI_SINC_PAGAR_FORNECEDOR
@@ -60,9 +66,10 @@ BEGIN
            DTVENCIMENTO,
            DTPAGAMENTO,
            VALOR,
-					 VALORAPAGAR,
+           VALORAPAGAR,
            CODFORNEC,
            NOTA,
+           CODBANCO,
            DT_UPDATE)
         VALUES
           (temp_rec.RECNUM,
@@ -71,9 +78,10 @@ BEGIN
            temp_rec.DTVENCIMENTO,
            temp_rec.DTPAGAMENTO,
            temp_rec.VALOR,
-					 temp_rec.VALORAPAGAR,
+           temp_rec.VALORAPAGAR,
            temp_rec.CODFORNEC,
            temp_rec.NOTA,
+           temp_rec.CODBANCO,
            SYSDATE);
       END IF;
     EXCEPTION
@@ -86,10 +94,9 @@ BEGIN
   END LOOP;
 
   COMMIT;
-  BEGIN
     -- Exclui os registros da tabela BI_SINC_PAGAR_FORNECEDOR que possuem DTPAGAMENTO NOT NULL
-    EXECUTE IMMEDIATE 'DELETE FROM BI_SINC_PAGAR_FORNECEDOR WHERE DTPAGAMENTO IS NOT NULL';
-  END;
+  EXECUTE IMMEDIATE 'DELETE FROM BI_SINC_PAGAR_FORNECEDOR WHERE DTPAGAMENTO IS NOT NULL OR NVL(CODBANCO, 0) IN (17, 20, 35, 50, 52, 53, 54)';
+
   -- Exclui os registros da tabela temporária TEMP criada;
   EXECUTE IMMEDIATE 'DELETE TEMP_PAGAR_FORNECEDOR';
 END;
