44270d1181861774b94f0bb228270cf5521626e8
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-07-15 15:21:25 -0300

    Update 01_PRC_SINC_FILIAL.sql

--- Filial/01_PRC_SINC_FILIAL.sql
+++ Filial/01_PRC_SINC_FILIAL.sql
@@ -1,12 +1,7 @@
 CREATE OR REPLACE PROCEDURE PRC_SINC_FILIAL AS
 BEGIN
 
-  -- Insere os resultados novos ou alterados na tabela TEMP
-  INSERT INTO TEMP_FILIAL
-    (CODFILIAL,
-     EMPRESA,
-     FILIAL,
-     ORDEM)
+FOR temp_rec IN (
     WITH FILIAIS AS
      (SELECT F.CODIGO CODFILIAL,
              NVL(F.NOMEREMETENTE, 'JC BROTHERS') EMPRESA,
@@ -18,10 +13,10 @@ BEGIN
       LEFT JOIN BI_SINC_FILIAL S ON S.CODFILIAL = F.CODFILIAL
      WHERE S.DT_UPDATE IS NULL
         OR NVL(S.EMPRESA, '0') <> F.EMPRESA
-        OR NVL(S.FILIAL, '0') <> F.FILIAL;
+        OR NVL(S.FILIAL, '0') <> F.FILIAL
+)
 
-  -- Atualiza ou insere os resultados na tabela BI_SINC conforme as condições mencionadas
-  FOR temp_rec IN (SELECT * FROM TEMP_FILIAL)
+  -- Atualiza ou insere os resultados na tabela BI_SINC conforme as condições mencionada
   
   LOOP
     BEGIN
@@ -57,6 +52,4 @@ BEGIN
 
   COMMIT;
 
-  -- Exclui os registros da tabela temporária TEMP criada;
-  EXECUTE IMMEDIATE 'DELETE TEMP_FILIAL';
 END;
