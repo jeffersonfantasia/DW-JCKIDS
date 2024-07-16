c0b794b052957306f28d63cab38a0cefcbeeb73f
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-07-16 18:28:37 -0300

    Update 01_PRC_SINC_CLIENTE.sql

--- Cliente/01_PRC_SINC_CLIENTE.sql
+++ Cliente/01_PRC_SINC_CLIENTE.sql
@@ -1,64 +1,74 @@
 CREATE OR REPLACE PROCEDURE PRC_SINC_CLIENTE AS
 BEGIN
 
-FOR temp_rec IN (
-
-    WITH CLIENTES AS
-     (SELECT C.CODCLI,
-             C.CLIENTE,
-             (CASE
-               WHEN R.DESCRICAO IS NULL THEN
-                ('C' || C.CODCLI)
-               ELSE
-                ('R' || C.CODREDE)
-             END) AS CODCLIREDE,
-             (CASE
-               WHEN R.DESCRICAO IS NULL THEN
-                ('C' || C.CODCLI || ' - ' || UPPER(C.CLIENTE))
-               ELSE
-                ('R' || C.CODREDE || ' - ' || UPPER(R.DESCRICAO))
-             END) AS CLIENTEREDE,
-             REGEXP_REPLACE(C.CGCENT,
-                            '([0-9]{2})([0-9]{3})([0-9]{3})([0-9]{4})',
-                            '\1.\2.\3/\4-') CNPJ,
-             REPLACE(C.CEPENT, '-', '') AS CEP,
-             C.ESTENT UF,
-             C.CODUSUR1 CODUSUR,
-             C.CODPRACA,
-             P.PRACA,
-             C.CODATV1 CODATIVIDADE,
-             A.RAMO RAMOATIVIDADE,
-             C.BLOQUEIODEFINITIVO,
-             C.BLOQUEIO BLOQUEIOATUAL,
-             C.LIMCRED LIMITECREDITO,
-             C.DTCADASTRO
-        FROM PCCLIENT C
-        LEFT JOIN PCREDECLIENTE R ON R.CODREDE = C.CODREDE
-        LEFT JOIN PCPRACA P ON P.CODPRACA = C.CODPRACA
-        LEFT JOIN PCATIVI A ON A.CODATIV = C.CODATV1)
-    SELECT C.*
-      FROM CLIENTES C
-      LEFT JOIN BI_SINC_CLIENTE S ON S.CODCLI = C.CODCLI
-     WHERE S.DT_UPDATE IS NULL
-        OR S.CLIENTE <> C.CLIENTE
-        OR S.CODCLIREDE <> C.CODCLIREDE
-        OR S.CLIENTEREDE <> C.CLIENTEREDE
-        OR S.CNPJ <> C.CNPJ
-        OR S.CEP <> C.CEP
-        OR S.UF <> C.UF
-        OR S.CODUSUR <> C.CODUSUR
-        OR S.CODPRACA <> C.CODPRACA
-        OR S.PRACA <> C.PRACA
-        OR S.CODATIVIDADE <> C.CODATIVIDADE
-        OR S.RAMOATIVIDADE <> C.RAMOATIVIDADE
-        OR S.BLOQUEIODEFINITIVO <> C.BLOQUEIODEFINITIVO
-        OR S.BLOQUEIOATUAL <> C.BLOQUEIOATUAL
-        OR S.LIMITECREDITO <> C.LIMITECREDITO
-        OR S.DTCADASTRO <> C.DTCADASTRO
-)
-
+  FOR temp_rec IN (
+                   
+                     WITH FILIAL AS
+                      (SELECT F.CODIGO CODFILIALJCCLUB,
+                              REPLACE(F.CEP, '-', '') CEP
+                         FROM PCFILIAL F
+                        WHERE INSTR(F.FANTASIA, 'LOJA') > 0),
+                     CLIENTES AS
+                      (SELECT C.CODCLI,
+                              C.CLIENTE,
+                              (CASE
+                                WHEN R.DESCRICAO IS NULL THEN
+                                 ('C' || C.CODCLI)
+                                ELSE
+                                 ('R' || C.CODREDE)
+                              END) AS CODCLIREDE,
+                              (CASE
+                                WHEN R.DESCRICAO IS NULL THEN
+                                 ('C' || C.CODCLI || ' - ' || UPPER(C.CLIENTE))
+                                ELSE
+                                 ('R' || C.CODREDE || ' - ' ||
+                                 UPPER(R.DESCRICAO))
+                              END) AS CLIENTEREDE,
+                              REGEXP_REPLACE(C.CGCENT,
+                                             '([0-9]{2})([0-9]{3})([0-9]{3})([0-9]{4})',
+                                             '\1.\2.\3/\4-') CNPJ,
+                              REPLACE(C.CEPENT, '-', '') AS CEP,
+                              C.ESTENT UF,
+                              NVL(F.CODFILIALJCCLUB, '99') CODFILIALJCCLUB,
+                              C.CODUSUR1 CODUSUR,
+                              C.CODPRACA,
+                              P.PRACA,
+                              C.CODATV1 CODATIVIDADE,
+                              A.RAMO RAMOATIVIDADE,
+                              C.BLOQUEIODEFINITIVO,
+                              C.BLOQUEIO BLOQUEIOATUAL,
+                              C.LIMCRED LIMITECREDITO,
+                              C.DTCADASTRO
+                         FROM PCCLIENT C
+                         LEFT JOIN PCREDECLIENTE R ON R.CODREDE = C.CODREDE
+                         LEFT JOIN PCPRACA P ON P.CODPRACA = C.CODPRACA
+                         LEFT JOIN PCATIVI A ON A.CODATIV = C.CODATV1
+                         LEFT JOIN FILIAL F ON F.CEP =
+                                               REPLACE(C.CEPENT, '-', ''))
+                     
+                     SELECT C.*
+                       FROM CLIENTES C
+                       LEFT JOIN BI_SINC_CLIENTE S ON S.CODCLI = C.CODCLI
+                      WHERE S.DT_UPDATE IS NULL
+                         OR S.CLIENTE <> C.CLIENTE
+                         OR S.CODCLIREDE <> C.CODCLIREDE
+                         OR S.CLIENTEREDE <> C.CLIENTEREDE
+                         OR S.CNPJ <> C.CNPJ
+                         OR S.CEP <> C.CEP
+                         OR S.UF <> C.UF
+                         OR NVL(S.CODFILIALJCCLUB,'-') <> C.CODFILIALJCCLUB
+                         OR S.CODUSUR <> C.CODUSUR
+                         OR S.CODPRACA <> C.CODPRACA
+                         OR S.PRACA <> C.PRACA
+                         OR S.CODATIVIDADE <> C.CODATIVIDADE
+                         OR S.RAMOATIVIDADE <> C.RAMOATIVIDADE
+                         OR S.BLOQUEIODEFINITIVO <> C.BLOQUEIODEFINITIVO
+                         OR S.BLOQUEIOATUAL <> C.BLOQUEIOATUAL
+                         OR S.LIMITECREDITO <> C.LIMITECREDITO
+                         OR S.DTCADASTRO <> C.DTCADASTRO)
+  
   -- Atualiza ou insere os resultados na tabela BI_SINC conforme as condições mencionadas
-
+  
   LOOP
     BEGIN
       UPDATE BI_SINC_CLIENTE
@@ -68,6 +78,7 @@ FOR temp_rec IN (
              CNPJ               = temp_rec.CNPJ,
              CEP                = temp_rec.CEP,
              UF                 = temp_rec.UF,
+             CODFILIALJCCLUB    = temp_rec.CODFILIALJCCLUB,
              CODUSUR            = temp_rec.CODUSUR,
              CODPRACA           = temp_rec.CODPRACA,
              PRACA              = temp_rec.PRACA,
@@ -80,8 +91,7 @@ FOR temp_rec IN (
              DT_UPDATE          = SYSDATE
        WHERE CODCLI = temp_rec.CODCLI;
     
-      IF SQL%NOTFOUND
-      THEN
+      IF SQL%NOTFOUND THEN
         INSERT INTO BI_SINC_CLIENTE
           (CODCLI,
            CLIENTE,
@@ -90,6 +100,7 @@ FOR temp_rec IN (
            CNPJ,
            CEP,
            UF,
+           CODFILIALJCCLUB,
            CODUSUR,
            CODPRACA,
            PRACA,
@@ -108,6 +119,7 @@ FOR temp_rec IN (
            temp_rec.CNPJ,
            temp_rec.CEP,
            temp_rec.UF,
+           temp_rec.CODFILIALJCCLUB,
            temp_rec.CODUSUR,
            temp_rec.CODPRACA,
            temp_rec.PRACA,
