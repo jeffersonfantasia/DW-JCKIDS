6382a0733a36b113d3e981119f51c377f97ab57d
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-12-02 16:15:55 -0300

    Update 01_PRC_SINC_FILIAL.sql

--- Filial/01_PRC_SINC_FILIAL.sql
+++ Filial/01_PRC_SINC_FILIAL.sql
@@ -1,35 +1,41 @@
 CREATE OR REPLACE PROCEDURE PRC_SINC_FILIAL AS
 BEGIN
 
-FOR temp_rec IN (
-    WITH FILIAIS AS (
-		SELECT F.CODIGO CODFILIAL,
-               G.NOME_GRUPOFILIAL EMPRESA,
-               NVL(F.FANTASIA, 'JC BROTHERS') FILIAL,
-               TO_NUMBER(F.CODIGO) ORDEM,
-               F.CODGRUPOFILIAL CODEMPRESA
-          FROM PCFILIAL F
-          LEFT JOIN PCGRUPOFILIAL G ON G.CODGRUPOFILIAL = F.CODGRUPOFILIAL)
-    SELECT F.*
-      FROM FILIAIS F
-      LEFT JOIN BI_SINC_FILIAL S ON S.CODFILIAL = F.CODFILIAL
-     WHERE S.DT_UPDATE IS NULL
-        OR NVL(S.EMPRESA, '0') <> F.EMPRESA
-        OR NVL(S.FILIAL, '0') <> F.FILIAL
-        OR NVL(S.CODEMPRESA, '0') <> F.CODEMPRESA
-)
-
+  FOR r IN (WITH FILIAIS AS
+               (SELECT F.CODIGO CODFILIAL,
+                      G.NOME_GRUPOFILIAL EMPRESA,
+                      NVL(F.FANTASIA, 'JC BROTHERS') FILIAL,
+                      TO_NUMBER(F.CODIGO) ORDEM,
+                      F.CODGRUPOFILIAL CODEMPRESA,
+                      NVL(F.CODFORNEC, 1) CODFORNEC,
+                      NVL(F.CODCLI, 4) CODCLI
+                 FROM PCFILIAL F
+                 LEFT JOIN PCGRUPOFILIAL G ON G.CODGRUPOFILIAL =
+                                              F.CODGRUPOFILIAL)
+              
+              SELECT F.*
+                FROM FILIAIS F
+                LEFT JOIN BI_SINC_FILIAL S ON S.CODFILIAL = F.CODFILIAL
+               WHERE S.DT_UPDATE IS NULL
+                  OR NVL(S.EMPRESA, '0') <> F.EMPRESA
+                  OR NVL(S.FILIAL, '0') <> F.FILIAL
+                  OR NVL(S.CODEMPRESA, '0') <> F.CODEMPRESA
+                  OR NVL(S.CODFORNEC, 0) <> F.CODFORNEC
+                  OR NVL(S.CODCLI, 0) <> F.CODCLI)
+  
   -- Atualiza ou insere os resultados na tabela BI_SINC conforme as condições mencionada
   
   LOOP
     BEGIN
       UPDATE BI_SINC_FILIAL
-         SET EMPRESA    = temp_rec.EMPRESA,
-             FILIAL     = temp_rec.FILIAL,
-             ORDEM      = temp_rec.ORDEM,
-             CODEMPRESA = temp_rec.CODEMPRESA,
+         SET EMPRESA    = r.EMPRESA,
+             FILIAL     = r.FILIAL,
+             ORDEM      = r.ORDEM,
+             CODEMPRESA = r.CODEMPRESA,
+             CODFORNEC  = r.CODFORNEC,
+             CODCLI     = r.CODCLI,
              DT_UPDATE  = SYSDATE
-       WHERE CODFILIAL  = temp_rec.CODFILIAL;
+       WHERE CODFILIAL = r.CODFILIAL;
     
       IF SQL%NOTFOUND THEN
         INSERT INTO BI_SINC_FILIAL
@@ -38,13 +44,17 @@ FOR temp_rec IN (
            FILIAL,
            ORDEM,
            CODEMPRESA,
+           CODFORNEC,
+           CODCLI,
            DT_UPDATE)
         VALUES
-          (temp_rec.CODFILIAL,
-           temp_rec.EMPRESA,
-           temp_rec.FILIAL,
-           temp_rec.CODEMPRESA,
-           temp_rec.ORDEM,
+          (r.CODFILIAL,
+           r.EMPRESA,
+           r.FILIAL,
+           r.ORDEM,
+           r.CODEMPRESA,
+           r.CODFORNEC,
+           r.CODCLI,
            SYSDATE);
       END IF;
     EXCEPTION
