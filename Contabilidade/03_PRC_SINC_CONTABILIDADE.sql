d819e49953e70cf481ec5ec4b3a5682b017f7196
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-12-17 15:52:44 -0300

    Update 03_PRC_SINC_CONTABILIDADE.sql

--- Contabilidade/03_PRC_SINC_CONTABILIDADE.sql
+++ Contabilidade/03_PRC_SINC_CONTABILIDADE.sql
@@ -23,8 +23,8 @@ BEGIN
                       M.DT_UPDATE
                  FROM BI_SINC_MOV_CONTABIL M
                 WHERE M.DTCANCEL IS NULL
-                      AND M.CONTADEBITO IS NOT NULL
-                      AND M.VALOR > 0),
+                  AND M.CONTADEBITO IS NOT NULL
+                  AND M.VALOR > 0),
               
               CONTA_CREDITO AS
                (SELECT M.CODLANC,
@@ -44,15 +44,11 @@ BEGIN
                       M.DT_UPDATE
                  FROM BI_SINC_MOV_CONTABIL M
                 WHERE M.DTCANCEL IS NULL
-                      AND M.CONTACREDITO IS NOT NULL
-                      AND M.VALOR > 0),
+                  AND M.CONTACREDITO IS NOT NULL
+                  AND M.VALOR > 0),
               
               MOVIMENTO_CONTABIL AS
-               (SELECT *
-                 FROM CONTA_DEBITO
-               UNION ALL
-               SELECT *
-                 FROM CONTA_CREDITO),
+               (SELECT * FROM CONTA_DEBITO UNION ALL SELECT * FROM CONTA_CREDITO),
               
               RESULTADO AS
                (SELECT M.CODLANC,
@@ -73,33 +69,23 @@ BEGIN
                       M.ENVIAR_CONTABIL,
                       M.DT_UPDATE
                  FROM MOVIMENTO_CONTABIL M
-                 LEFT JOIN BI_SINC_PLANO_CONTAS_JC C ON C.CODGERENCIAL =
-                                                        M.CODGERENCIAL)
+                 LEFT JOIN BI_SINC_PLANO_CONTAS_JC C ON C.CODGERENCIAL = M.CODGERENCIAL)
               
               SELECT M.*
                 FROM RESULTADO M
-                LEFT JOIN BI_SINC_CONTABILIDADE S ON M.CODLANC =
-                                                     S.CODLANC
-                                                     AND
-                                                     S.IDENTIFICADOR =
-                                                     M.IDENTIFICADOR
+                LEFT JOIN BI_SINC_CONTABILIDADE S ON M.CODLANC = S.CODLANC
+                                                 AND S.IDENTIFICADOR = M.IDENTIFICADOR
+                                                 AND S.CODCC = M.CODCC
                WHERE 1 = 1
-                     AND M.DATA >= vDATA_MOV_INCREMENTAL
-                     AND (S.DT_UPDATE IS NULL
-                      OR S.CODEMPRESA <> M.CODEMPRESA 
-                      OR NVL(S.DATA, TO_DATE('01/01/1889', 'DD/MM/YYYY')) <> M.DATA
-                      OR NVL(S.TIPOLANCAMENTO, 0) <> NVL(M.TIPOLANCAMENTO, 0)
-                      OR NVL(S.DOCUMENTO, 0) <> NVL(M.DOCUMENTO, 0)
-                      OR NVL(S.OPERACAO, 0) <> NVL(M.OPERACAO, 0)
-                      OR NVL(S.CODGERENCIAL, 0) <> NVL(M.CODGERENCIAL, 0)
-                      OR NVL(S.CODCC, '0') <> NVL(M.CODCC, '0')
-                      OR NVL(S.CODDRE, '0') <>  NVL(M.CODDRE, '0')
-                      OR NVL(S.CODCONTABIL, '0') <>  NVL(M.CODCONTABIL, '0')
-                      OR S.ATIVIDADE <> M.ATIVIDADE
-                      OR S.HISTORICO <> M.HISTORICO 
-                      OR S.VALOR <> M.VALOR
-                      OR S.ORIGEM <> M.ORIGEM
-                      OR NVL(S.ENVIAR_CONTABIL, '0') <> M.ENVIAR_CONTABIL))
+                 AND M.DATA >= vDATA_MOV_INCREMENTAL
+                 AND (S.DT_UPDATE IS NULL OR S.CODEMPRESA <> M.CODEMPRESA OR
+                     NVL(S.DATA, TO_DATE('01/01/1889', 'DD/MM/YYYY')) <> M.DATA OR
+                     NVL(S.TIPOLANCAMENTO, 0) <> NVL(M.TIPOLANCAMENTO, 0) OR NVL(S.DOCUMENTO, 0) <> NVL(M.DOCUMENTO, 0) OR
+                     NVL(S.OPERACAO, 0) <> NVL(M.OPERACAO, 0) OR NVL(S.CODGERENCIAL, 0) <> NVL(M.CODGERENCIAL, 0) OR
+                     NVL(S.CODCC, '99') <> NVL(M.CODCC, '0') OR NVL(S.CODDRE, '0') <> NVL(M.CODDRE, '0') OR
+                     NVL(S.CODCONTABIL, '0') <> NVL(M.CODCONTABIL, '0') OR S.ATIVIDADE <> M.ATIVIDADE OR
+                     S.HISTORICO <> M.HISTORICO OR S.VALOR <> M.VALOR OR S.ORIGEM <> M.ORIGEM OR
+                     NVL(S.ENVIAR_CONTABIL, '0') <> M.ENVIAR_CONTABIL))
   
   -- Atualiza ou insere os resultados na tabela BI_SINC conforme as condições mencionadas
   
@@ -166,14 +152,12 @@ BEGIN
     EXCEPTION
       WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Erro encontrado: ' || SQLERRM);
-        RAISE_APPLICATION_ERROR(-20000,
-                                'Erro durante a insercao na tabela: ' ||
-                                SQLERRM);
+        RAISE_APPLICATION_ERROR(-20000, 'Erro durante a insercao na tabela: ' || SQLERRM);
     END;
   END LOOP;
 
   COMMIT;
-  
+
   BEGIN
     EXECUTE IMMEDIATE 'DELETE FROM BI_SINC_CONTABILIDADE
  WHERE (CODLANC, IDENTIFICADOR) IN
@@ -185,7 +169,7 @@ BEGIN
                                               S.IDENTIFICADOR
          WHERE M.CODLANC IS NULL)';
   END;
-  
+
   COMMIT;
-  
+
 END;
