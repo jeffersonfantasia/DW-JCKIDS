a8a1c83433b6a425a3c4ba83b6aa16332e0ff709
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2025-02-24 12:10:01 -0300

    Update 02_VIEW_BI_SINC_LANCAMENTOS.sql

--- Lancamentos/02_VIEW_BI_SINC_LANCAMENTOS.sql
+++ Lancamentos/02_VIEW_BI_SINC_LANCAMENTOS.sql
@@ -2,33 +2,42 @@ CREATE OR REPLACE VIEW VIEW_BI_SINC_LANCAMENTOS AS
 
   SELECT DISTINCT 'P' MOVIMENTO,
                   L.CODFILIAL,
+                  L.DTLANC,
                   L.DTVENCIMENTO,
                   L.DTVENCUTIL,
                   NVL(L.DTCOMPENSACAO, L.DTPAGAMENTO) DTCOMPENSACAO,
+                  L.CODBANCO,
                   L.CODFLUXO,
                   L.CODCONTA,
                   ((L.VALOR - L.VLDESCONTO - L.VLDEVOLUCAO) * -1) VALOR,
                   DECODE(L.TIPOPARCEIRO, 'F', L.CODFORNEC, 0) CODFORNEC,
                   DECODE(L.TIPOPARCEIRO, 'C', L.CODFORNEC, 0) CODCLI,
-                  L.NUMNOTA,
+                  TO_CHAR(L.NUMNOTA) NUMNOTA,
                   'D' CODCOB,
                   0 CODUSUR,
                   0 CODINADIMPLENCIA,
+                  0 DIASVENCIDOS,
                   (CASE
                     WHEN L.HISTORICO IS NOT NULL THEN
                      'Nº Lanc: ' || L.RECNUM || ' - ' || L.HISTORICO
                     ELSE
                      'Nº Lanc: ' || L.RECNUM
-                  END) HISTORICO
+                  END) HISTORICO,
+                  L.DT_UPDATE
     FROM BI_SINC_LANC_PAGAR_BASE L
-		WHERE L.GRUPOCONTA NOT IN (110,200,900)
-		AND L.CODCONTA NOT IN (37, 2100)
+   WHERE L.GRUPOCONTA NOT IN (110, 200, 900)
+     AND L.CODCONTA NOT IN (37, 2100)
+     AND NOT (NVL(L.CODROTINABAIXA, 0) = 746 AND L.CODBANCO IS NULL)
+     AND NOT (L.DTPAGAMENTO IS NOT NULL AND L.NUMTRANS IS NULL)
+     AND L.DTESTORNOBAIXA IS NULL
   UNION ALL
   SELECT 'R' MOVIMENTO,
          R.CODFILIAL,
+         R.DTEMISSAO DTLANC,
          R.DTVENCIMENTO,
          R.DTVENCUTIL,
          NVL(R.DTCOMPENSACAO, R.DTPAGAMENTO) DTCOMPENSACAO,
+         R.CODBANCO,
          R.CODFLUXO,
          0 CODCONTA,
          (CASE
@@ -39,17 +48,18 @@ CREATE OR REPLACE VIEW VIEW_BI_SINC_LANCAMENTOS AS
          END) VALOR,
          0 CODFORNEC,
          R.CODCLI,
-         R.NUMNOTA,
+         (R.NUMNOTA || '-' || LPAD(R.PREST, 2, '0')) NUMNOTA,
          R.CODCOB,
          R.CODUSUR,
          R.CODINADIMPLENCIA,
+         R.DIASVENCIDOS,
          (CASE
            WHEN R.HISTORICO IS NOT NULL THEN
-            'Nº Transvenda: ' || R.NUMTRANSVENDA || '-' || LPAD(R.PREST, 2, '0') || ' - ' || R.HISTORICO
+            'Nº Transvenda: ' || R.NUMTRANSVENDA || ' - ' || R.HISTORICO
            ELSE
-            'Nº Transvenda: ' || R.NUMTRANSVENDA || '-' || LPAD(R.PREST, 2, '0')
-         END) HISTORICO
+            'Nº Transvenda: ' || R.NUMTRANSVENDA
+         END) HISTORICO,
+         R.DT_UPDATE
     FROM BI_SINC_LANC_RECEBER_BASE R
-   WHERE R.DTDESD IS NULL
-     AND R.DTESTORNO IS NULL
-		 AND R.CODCOB NOT IN ('CRED');
+   WHERE R.CODCOB NOT IN ('DESD', 'PERD')
+     AND NVL(R.VLRECEBIDO,1) <> 0;
