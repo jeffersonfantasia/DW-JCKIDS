613298b6acc868b23987c82145f7762ad6c5879e
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2025-03-20 14:58:25 -0300

    Update 02_PRC_SINC_CONTABILIDADE.sql

--- Contabilidade/02_PRC_SINC_CONTABILIDADE.sql
+++ Contabilidade/02_PRC_SINC_CONTABILIDADE.sql
@@ -2,26 +2,27 @@ CREATE OR REPLACE PROCEDURE PRC_SINC_CONTABILIDADE AS
 
 BEGIN
   FOR r IN (SELECT M.*
-                FROM VIEW_BI_SINC_MOV_CONTABIL_DC M
-                LEFT JOIN BI_SINC_CONTABILIDADE S ON M.CODLANC = S.CODLANC
-                                                 AND S.IDENTIFICADOR = M.IDENTIFICADOR
-                                                 AND S.OPERACAO = M.OPERACAO
-               WHERE S.DT_UPDATE IS NULL
-                  OR S.CODEMPRESA <> M.CODEMPRESA
-                  OR NVL(S.DATA, TO_DATE('01/01/1889', 'DD/MM/YYYY')) <>
-                     NVL(M.DATA, TO_DATE('01/01/1889', 'DD/MM/YYYY'))
-                  OR NVL(S.TIPOLANCAMENTO, 0) <> NVL(M.TIPOLANCAMENTO, 0)
-                  OR NVL(S.DOCUMENTO, 0) <> NVL(M.DOCUMENTO, 0)
-                  OR NVL(S.OPERACAO, 0) <> NVL(M.OPERACAO, 0)
-                  OR NVL(S.CODGERENCIAL, 0) <> NVL(M.CODGERENCIAL, 0)
-                  OR NVL(S.CODCC, '99') <> NVL(M.CODCC, '0')
-                  OR NVL(S.CODDRE, '0') <> NVL(M.CODDRE, '0')
-                  OR NVL(S.CODCONTABIL, '0') <> NVL(M.CODCONTABIL, '0')
-                  OR S.ATIVIDADE <> M.ATIVIDADE
-                  OR S.HISTORICO <> M.HISTORICO
-                  OR S.VALOR <> M.VALOR
-                  OR S.ORIGEM <> M.ORIGEM
-                  OR NVL(S.ENVIAR_CONTABIL, '0') <> M.ENVIAR_CONTABIL)
+              FROM VIEW_BI_SINC_MOV_CONTABIL_DC M
+              LEFT JOIN BI_SINC_CONTABILIDADE S ON M.CODLANC = S.CODLANC
+                                               AND S.OPERACAO = M.OPERACAO
+             WHERE S.DT_UPDATE IS NULL
+                OR S.CODEMPRESA <> M.CODEMPRESA
+                OR NVL(S.DATA, TO_DATE('01/01/1889', 'DD/MM/YYYY')) <> NVL(M.DATA, TO_DATE('01/01/1889', 'DD/MM/YYYY'))
+                OR NVL(S.TIPOLANCAMENTO, 0) <> NVL(M.TIPOLANCAMENTO, 0)
+                OR NVL(S.IDENTIFICADOR, 0) <> NVL(M.IDENTIFICADOR, 0)
+                OR NVL(S.DOCUMENTO, 0) <> NVL(M.DOCUMENTO, 0)
+                OR NVL(S.OPERACAO, 0) <> NVL(M.OPERACAO, 0)
+                OR NVL(S.CODGERENCIAL, 0) <> NVL(M.CODGERENCIAL, 0)
+                OR NVL(S.CODCC, '99') <> NVL(M.CODCC, '0')
+                OR NVL(S.CODDRE, '0') <> NVL(M.CODDRE, '0')
+                OR NVL(S.CODCONTABIL, '0') <> NVL(M.CODCONTABIL, '0')
+                OR NVL(S.IDGERENCIAL, '0') <> NVL(M.IDGERENCIAL, '0')
+                OR NVL(S.IDCONTABIL, '0') <> NVL(M.IDCONTABIL, '0')
+                OR S.ATIVIDADE <> M.ATIVIDADE
+                OR S.HISTORICO <> M.HISTORICO
+                OR S.VALOR <> M.VALOR
+                OR S.ORIGEM <> M.ORIGEM
+                OR NVL(S.ENVIAR_CONTABIL, '0') <> M.ENVIAR_CONTABIL)
   
   -- Atualiza ou insere os resultados na tabela BI_SINC conforme as condições mencionadas
   
@@ -31,12 +32,14 @@ BEGIN
          SET CODEMPRESA      = r.CODEMPRESA,
              DATA            = r.DATA,
              TIPOLANCAMENTO  = r.TIPOLANCAMENTO,
+             IDENTIFICADOR   = r.IDENTIFICADOR,
              DOCUMENTO       = r.DOCUMENTO,
-             OPERACAO        = r.OPERACAO,
              CODGERENCIAL    = r.CODGERENCIAL,
              CODCC           = r.CODCC,
              CODDRE          = r.CODDRE,
              CODCONTABIL     = r.CODCONTABIL,
+             IDGERENCIAL     = r.IDGERENCIAL,
+             IDCONTABIL      = r.IDCONTABIL,
              ATIVIDADE       = r.ATIVIDADE,
              HISTORICO       = r.HISTORICO,
              VALOR           = r.VALOR,
@@ -44,7 +47,6 @@ BEGIN
              ENVIAR_CONTABIL = r.ENVIAR_CONTABIL,
              DT_UPDATE       = SYSDATE
        WHERE CODLANC = r.CODLANC
-         AND IDENTIFICADOR = r.IDENTIFICADOR
          AND OPERACAO = r.OPERACAO;
     
       IF SQL%NOTFOUND THEN
@@ -60,6 +62,8 @@ BEGIN
            CODCC,
            CODDRE,
            CODCONTABIL,
+           IDGERENCIAL,
+           IDCONTABIL,
            ATIVIDADE,
            HISTORICO,
            VALOR,
@@ -78,6 +82,8 @@ BEGIN
            r.CODCC,
            r.CODDRE,
            r.CODCONTABIL,
+           r.IDGERENCIAL,
+           r.IDCONTABIL,
            r.ATIVIDADE,
            r.HISTORICO,
            r.VALOR,
@@ -102,7 +108,7 @@ BEGIN
            LEFT JOIN VIEW_BI_SINC_MOV_CONTABIL_DC M ON M.CODLANC = S.CODLANC
           WHERE M.CODLANC IS NULL)';
   END;
-  
+
   COMMIT;
 
 END;
