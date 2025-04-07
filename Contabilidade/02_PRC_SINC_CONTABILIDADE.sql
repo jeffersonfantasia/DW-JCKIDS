9ec6eb388a615a43263ac6e10caf7045e9f94fdf
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2025-04-07 15:39:31 -0300

    Update 02_PRC_SINC_CONTABILIDADE.sql

--- Contabilidade/02_PRC_SINC_CONTABILIDADE.sql
+++ Contabilidade/02_PRC_SINC_CONTABILIDADE.sql
@@ -1,28 +1,36 @@
 CREATE OR REPLACE PROCEDURE PRC_SINC_CONTABILIDADE AS
 
+  -----------------------DATAS DE ATUALIZACAO
+  --vDATA_MOV_INCREMENTAL DATE := TRUNC(SYSDATE) - 90;
+  vDATA_MOV_INCREMENTAL DATE := TO_DATE('01/01/2020', 'DD/MM/YYYY');
+
 BEGIN
-  FOR r IN (SELECT M.*
-              FROM VIEW_BI_SINC_MOV_CONTABIL_DC M
-              LEFT JOIN BI_SINC_CONTABILIDADE S ON M.CODLANC = S.CODLANC
-                                               AND S.OPERACAO = M.OPERACAO
-             WHERE S.DT_UPDATE IS NULL
-                OR S.CODEMPRESA <> M.CODEMPRESA
-                OR NVL(S.DATA, TO_DATE('01/01/1889', 'DD/MM/YYYY')) <> NVL(M.DATA, TO_DATE('01/01/1889', 'DD/MM/YYYY'))
-                OR NVL(S.TIPOLANCAMENTO, 0) <> NVL(M.TIPOLANCAMENTO, 0)
-                OR NVL(S.IDENTIFICADOR, 0) <> NVL(M.IDENTIFICADOR, 0)
-                OR NVL(S.DOCUMENTO, 0) <> NVL(M.DOCUMENTO, 0)
-                OR NVL(S.OPERACAO, 0) <> NVL(M.OPERACAO, 0)
-                OR NVL(S.CODGERENCIAL, 0) <> NVL(M.CODGERENCIAL, 0)
-                OR NVL(S.CODCC, '99') <> NVL(M.CODCC, '0')
-                OR NVL(S.CODDRE, '0') <> NVL(M.CODDRE, '0')
-                OR NVL(S.CODCONTABIL, '0') <> NVL(M.CODCONTABIL, '0')
-                OR NVL(S.IDGERENCIAL, '0') <> NVL(M.IDGERENCIAL, '0')
-                OR NVL(S.IDCONTABIL, '0') <> NVL(M.IDCONTABIL, '0')
-                OR S.ATIVIDADE <> M.ATIVIDADE
-                OR S.HISTORICO <> M.HISTORICO
-                OR S.VALOR <> M.VALOR
-                OR S.ORIGEM <> M.ORIGEM
-                OR NVL(S.ENVIAR_CONTABIL, '0') <> M.ENVIAR_CONTABIL)
+  FOR r IN (WITH CONTABILIDADE AS
+               (SELECT C.* FROM VIEW_BI_SINC_MOV_CONTABIL_DC C WHERE C.DATA >= vDATA_MOV_INCREMENTAL)
+              
+              SELECT M.*
+                FROM CONTABILIDADE M
+                LEFT JOIN BI_SINC_CONTABILIDADE S ON M.CODLANC = S.CODLANC
+                                                 AND S.OPERACAO = M.OPERACAO
+               WHERE S.DT_UPDATE IS NULL
+                  OR S.CODEMPRESA <> M.CODEMPRESA
+                  OR NVL(S.DATA, TO_DATE('01/01/1889', 'DD/MM/YYYY')) <>
+                     NVL(M.DATA, TO_DATE('01/01/1889', 'DD/MM/YYYY'))
+                  OR NVL(S.TIPOLANCAMENTO, 0) <> NVL(M.TIPOLANCAMENTO, 0)
+                  OR NVL(S.IDENTIFICADOR, 0) <> NVL(M.IDENTIFICADOR, 0)
+                  OR NVL(S.DOCUMENTO, 0) <> NVL(M.DOCUMENTO, 0)
+                  OR NVL(S.OPERACAO, 0) <> NVL(M.OPERACAO, 0)
+                  OR NVL(S.CODGERENCIAL, 0) <> NVL(M.CODGERENCIAL, 0)
+                  OR NVL(S.CODCC, '99') <> NVL(M.CODCC, '0')
+                  OR NVL(S.CODDRE, '0') <> NVL(M.CODDRE, '0')
+                  OR NVL(S.CODCONTABIL, '0') <> NVL(M.CODCONTABIL, '0')
+                  OR NVL(S.IDGERENCIAL, '0') <> NVL(M.IDGERENCIAL, '0')
+                  OR NVL(S.IDCONTABIL, '0') <> NVL(M.IDCONTABIL, '0')
+                  OR S.ATIVIDADE <> M.ATIVIDADE
+                  OR S.HISTORICO <> M.HISTORICO
+                  OR S.VALOR <> M.VALOR
+                  OR S.ORIGEM <> M.ORIGEM
+                  OR NVL(S.ENVIAR_CONTABIL, '0') <> M.ENVIAR_CONTABIL)
   
   -- Atualiza ou insere os resultados na tabela BI_SINC conforme as condições mencionadas
   
