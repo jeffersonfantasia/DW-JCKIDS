ad2101dab65b9070785e25295303e8595e3e2dae
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2025-05-04 15:34:37 -0300

    Update 01_PRC_SINC_HIST_ESTOQUE.sql

--- Estoque/01_PRC_SINC_HIST_ESTOQUE.sql
+++ Estoque/01_PRC_SINC_HIST_ESTOQUE.sql
@@ -1,34 +1,37 @@
 CREATE OR REPLACE PROCEDURE PRC_SINC_HIST_ESTOQUE AS
 
   -----------------------DATAS DE ATUALIZACAO
-  vDATA_MOV_INCREMENTAL DATE := TRUNC(SYSDATE) - 7;
+  --vDATA_MOV_INCREMENTAL DATE := TRUNC(SYSDATE) - 1;
   --vDATA_MOV_INCREMENTAL DATE := TO_DATE('01/01/2020', 'DD/MM/YYYY');
 
 BEGIN
   FOR r IN (WITH ESTOQUE AS
                (SELECT E.DATA,
                       E.CODFILIAL,
-                      E.CODPROD,
-                      NVL(E.QTEST, 0) QTCONTABIL,
-                      NVL(E.QTESTGER, 0) QTGERENCIAL,
-                      ROUND(ROUND(E.QTEST, 3) * ROUND(E.CUSTOCONT, 3), 2) VLESTOQUECONTABIL,
-                      ROUND(ROUND(E.QTESTGER, 3) * ROUND(E.CUSTOFIN, 3), 2) VLESTOQUEFINANCEIRO,
-                      ROUND(ROUND(E.QTESTGER, 3) * ROUND(E.CUSTOREP, 3), 2) VLESTOQUEGERENCIAL
+                      SUM(ROUND(ROUND(E.QTEST, 3) * ROUND(E.CUSTOCONT, 3), 2)) VLESTOQUECONTABIL,
+                      SUM(ROUND(ROUND(E.QTESTGER, 3) * ROUND(E.CUSTOFIN, 3), 2)) VLESTOQUEFINANCEIRO,
+                      SUM(ROUND(ROUND(E.QTESTGER, 3) * ROUND(E.CUSTOREP, 3), 2)) VLESTOQUEGERENCIAL
                  FROM PCHISTEST E
-                WHERE E.DATA >= vDATA_MOV_INCREMENTAL
+                WHERE E.DATA >= TRUNC(SYSDATE) - 1
                   AND NVL(E.TIPOMERCDEPTO, 'X') <> 'IM'
                   AND NVL(E.TIPOMERCDEPTO, 'X') <> 'CI'
                   AND NVL(E.TIPOMERC, 'X') NOT IN ('MC', 'ME', 'PB')
-                  AND ((ROUND(E.QTEST, 3) > 0 OR ROUND(E.QTESTGER, 3) > 0)))
+                GROUP BY E.DATA,
+                         E.CODFILIAL
+               UNION ALL
+               SELECT TRUNC(SYSDATE) DATA,
+                      E.CODFILIAL,
+                      ROUND(SUM(VLESTOQUECONTABIL), 2) VLESTOQUECONTABIL,
+                      ROUND(SUM(VLESTOQUEFINANCEIRO), 2) VLESTOQUEFINANCEIRO,
+                      ROUND(SUM(VLESTOQUEGERENCIAL), 2) VLESTOQUEGERENCIAL
+                 FROM BI_SINC_ESTOQUE E
+                GROUP BY E.CODFILIAL)
               
               SELECT E.*
                 FROM ESTOQUE E
                 LEFT JOIN BI_SINC_HIST_ESTOQUE S ON S.DATA = E.DATA
                                                 AND S.CODFILIAL = E.CODFILIAL
-                                                AND S.CODPROD = E.CODPROD
                WHERE S.DT_UPDATE IS NULL
-                  OR S.QTCONTABIL <> E.QTCONTABIL
-                  OR S.QTGERENCIAL <> E.QTGERENCIAL
                   OR S.VLESTOQUECONTABIL <> E.VLESTOQUECONTABIL
                   OR S.VLESTOQUEFINANCEIRO <> E.VLESTOQUEFINANCEIRO
                   OR S.VLESTOQUEGERENCIAL <> E.VLESTOQUEGERENCIAL)
@@ -38,23 +41,17 @@ BEGIN
   LOOP
     BEGIN
       UPDATE BI_SINC_HIST_ESTOQUE
-         SET QTCONTABIL          = r.QTCONTABIL,
-             QTGERENCIAL         = r.QTGERENCIAL,
-             VLESTOQUECONTABIL   = r.VLESTOQUECONTABIL,
+         SET VLESTOQUECONTABIL   = r.VLESTOQUECONTABIL,
              VLESTOQUEFINANCEIRO = r.VLESTOQUEFINANCEIRO,
              VLESTOQUEGERENCIAL  = r.VLESTOQUEGERENCIAL,
              DT_UPDATE           = SYSDATE
        WHERE DATA = r.DATA
-         AND CODFILIAL = r.CODFILIAL
-         AND CODPROD = r.CODPROD;
+         AND CODFILIAL = r.CODFILIAL;
     
       IF SQL%NOTFOUND THEN
         INSERT INTO BI_SINC_HIST_ESTOQUE
           (DATA,
            CODFILIAL,
-           CODPROD,
-           QTCONTABIL,
-           QTGERENCIAL,
            VLESTOQUECONTABIL,
            VLESTOQUEFINANCEIRO,
            VLESTOQUEGERENCIAL,
@@ -62,9 +59,6 @@ BEGIN
         VALUES
           (r.DATA,
            r.CODFILIAL,
-           r.CODPROD,
-           r.QTCONTABIL,
-           r.QTGERENCIAL,
            r.VLESTOQUECONTABIL,
            r.VLESTOQUEFINANCEIRO,
            r.VLESTOQUEGERENCIAL,
