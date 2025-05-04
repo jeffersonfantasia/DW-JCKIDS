16ee4c120b9b018664def55cbbebf7b3ff20128a
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2025-05-04 15:34:32 -0300

    Update 04_VIEW_BI_SINC_CONTABILIDADE_DRE.sql

--- Contabilidade/04_VIEW_BI_SINC_CONTABILIDADE_DRE.sql
+++ Contabilidade/04_VIEW_BI_SINC_CONTABILIDADE_DRE.sql
@@ -3,6 +3,7 @@ CREATE OR REPLACE VIEW VIEW_BI_SINC_CONTABILIDADE_DRE AS
   WITH DRE_AGG AS
    (SELECT C.CODDRE,
            C.CODEMPRESA,
+           C.CODFILIAL,
            TO_DATE(TRUNC(TO_DATE(C.DATA, 'DD/MM/YYYY'), 'MM')) DATA,
            C.CODCC,
            C.VALOR
@@ -11,11 +12,13 @@ CREATE OR REPLACE VIEW VIEW_BI_SINC_CONTABILIDADE_DRE AS
   
   SELECT CODDRE,
          CODEMPRESA,
+         CODFILIAL,
          DATA,
          CODCC,
          (SUM(VALOR) * -1) VALOR
     FROM DRE_AGG
    GROUP BY CODDRE,
             CODEMPRESA,
+            CODFILIAL,
             DATA,
             CODCC;
