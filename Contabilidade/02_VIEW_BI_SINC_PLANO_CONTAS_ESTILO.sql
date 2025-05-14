9c48f6c93eb96f3bb207649a6f3b3f598231e39f
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2025-05-14 14:18:30 -0300

    Update 02_VIEW_BI_SINC_PLANO_CONTAS_ESTILO.sql

--- Contabilidade/02_VIEW_BI_SINC_PLANO_CONTAS_ESTILO.sql
+++ Contabilidade/02_VIEW_BI_SINC_PLANO_CONTAS_ESTILO.sql
@@ -1,40 +1,40 @@
---CREATE OR REPLACE VIEW VIEW_BI_SINC_PLANO_CONTAS_ESTILO AS
+CREATE OR REPLACE VIEW VIEW_BI_SINC_PLANO_CONTAS_ESTILO AS
 
-    WITH EMPRESA AS
-     (SELECT F.CODEMPRESA FROM BI_SINC_FILIAL F GROUP BY F.CODEMPRESA),
-    
-    PLANO_JC_EMPRESAS AS
-     (SELECT E.CODEMPRESA,
+      WITH EMPRESA AS
+       (SELECT F.CODEMPRESA FROM BI_SINC_FILIAL F GROUP BY F.CODEMPRESA),
+      
+      PLANO_JC_EMPRESAS AS
+       (SELECT E.CODEMPRESA,
+               C.CONTA,
+               C.CODCONTA CODCONTABIL,
+               C.CODN1,
+               C.CONTAN1,
+               C.CODN2,
+               C.CONTAN2,
+               C.CODN3,
+               C.CONTAN3,
+               C.CODN4,
+               C.CONTAN4
+          FROM BI_SINC_PLANO_CONTAS_ESTILO C
+         CROSS JOIN EMPRESA E
+         ORDER BY C.ORDEM,
+                  E.CODEMPRESA),
+      
+      PLANO_UNIFICADO AS
+       (SELECT * FROM VIEW_BI_SINC_FORNEC_PLANO_CONTAS_ESTILO UNION ALL SELECT * FROM PLANO_JC_EMPRESAS)
+      
+      
+      SELECT (C.CODCONTABIL || '-' || C.CODEMPRESA) IDCONTABIL,
+             C.CODEMPRESA,
              C.CONTA,
-             C.CODCONTA,
+             C.CODCONTABIL,
+             C.CODN1,
              C.CONTAN1,
+             C.CODN2,
              C.CONTAN2,
+             C.CODN3,
              C.CONTAN3,
+             C.CODN4,
              C.CONTAN4
-        FROM BI_SINC_PLANO_CONTAS_ESTILO C
-       CROSS JOIN EMPRESA E
-       ORDER BY C.ORDEM,
-                E.CODEMPRESA)
-    
-  /*  PLANO_UNIFICADO AS
-     (SELECT * FROM VIEW_BI_SINC_FORNEC_PLANO_CONTAS UNION ALL SELECT * FROM PLANO_JC_EMPRESAS)*/
-		 
-		 SELECT * FROM PLANO_JC_EMPRESAS;
-    
-    SELECT (C.CODCONTA || '-' || C.CODEMPRESA) IDCONTABIL,
-           C.CODCLASSIFICA,
-           C.CONTA,
-           C.NIVEL,
-           C.TIPOCONTA,
-           C.CODGERENCIAL,
-           C.CODCONTABIL,
-           C.CODBALANCO,
-           C.CODDRE,
-           C.CODEBTIDA,
-           C.CONTAN1,
-           C.CONTAN2,
-           C.CONTAN3,
-           C.CONTAN4,
-           C.CONTAN5
-      FROM PLANO_UNIFICADO C
-     ORDER BY CODCLASSIFICA;
+        FROM PLANO_UNIFICADO C
+       ORDER BY C.CODN1, TO_NUMBER(C.CODCONTABIL), C.CODEMPRESA;
